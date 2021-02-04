21ee9b2eb1e38ce317d4426eb1e76fc170b7fa82
Kurylo Alexey <kurylo1.20@gmail.com>
2021-02-04 18:47:34 +0300

    --m

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("2Hello World2");
         System.out.println("change IDEA");
-        
+        System.out.println("change IDEA2");
     }
 }
